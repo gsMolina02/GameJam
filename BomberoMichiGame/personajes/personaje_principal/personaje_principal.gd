afa97cace18d548253b9d2928ca115a8406cdc05
Daniela Lissette Tituaña Moreno <dltituana1@espe.edu.ec>
2025-10-18 15:10:41 -0500

    giro personaje

--- BomberoMichiGame/personajes/personaje_principal/personaje_principal.gd
+++ BomberoMichiGame/personajes/personaje_principal/personaje_principal.gd
@@ -2,3 +2,11 @@ extends "res://personajes/personaje_base.gd"
 
 func _physics_process(delta):
 	mover_personaje(delta)
+	
+	# Girar el sprite horizontalmente según la dirección
+	if velocity.x < 0:
+		$Sprite2D.scale.x = -1  # Mirar a la izquierda
+		print("Izquierda")
+	elif velocity.x > 0:
+		$Sprite2D.scale.x = 1   # Mirar a la derecha
+		print("Derecha")
