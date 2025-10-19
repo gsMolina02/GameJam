0a9a30fc2bf48d4c8b6981422e0a62346debd492
AxelHerrera4 <93292952+AxelHerrera4@users.noreply.github.com>
2025-10-18 23:34:53 -0500

    HUD y conteo de daño

--- BomberoMichiGame/personajes/personaje_principal/personaje_principal.gd
+++ BomberoMichiGame/personajes/personaje_principal/personaje_principal.gd
@@ -2,3 +2,10 @@ extends "res://personajes/personaje_base.gd"
 
 func _physics_process(delta):
 	mover_personaje(delta)
+
+func _ready():
+	# Llama a la inicialización del padre (conexión Hitbox, init vida, etc.)
+	super._ready()
+	# Añadir al grupo para que HUD/etc. nos encuentre
+	add_to_group("player_main")
+	# (No necesitas emitir la señal aquí porque la base ya la emite al inicializar vida)
